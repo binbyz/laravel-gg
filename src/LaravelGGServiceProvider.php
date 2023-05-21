d3bdd0dd8e5351a3d4d923864c3e7c573344bd15
byzz <eyedroot@gmail.com>
2023-05-21 20:50:07 +0900

    update: composer.json (illuminate/support)

---
+++ src/LaravelGGServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Beaverlabs\LaravelGG;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelGGServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+    }
+
+    public function boot()
+    {
+    }
+}
