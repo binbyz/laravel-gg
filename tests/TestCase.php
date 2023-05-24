55537f23a80e5c63670bce0b769b24759f8cc64b
byzz <eyedroot@gmail.com>
2023-05-24 16:34:52 +0900

    update

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,8 +6,6 @@ use Beaverlabs\LaravelGG\LaravelGGServiceProvider;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
-//use PHPUnit\Framework\TestCase as PHPUnitTestCase;
-//use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 class TestCase extends OrchestraTestCase
 {
