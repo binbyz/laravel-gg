c68f0a1475a9fcbce6c242663e7167ae41c2910d
byzz <eyedroot@gmail.com>
2023-05-28 20:49:35 +0900

    update: TestCase.php (laravel application)

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,23 +3,56 @@
 namespace Beaverlabs\LaravelGG\Tests;
 
 use Beaverlabs\LaravelGG\Exceptions\BindException;
+use Beaverlabs\LaravelGG\LaravelGGServiceProvider;
+use Illuminate\Config\Repository;
+use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class TestCase extends \PHPUnit\Framework\TestCase
+class TestCase extends \Illuminate\Foundation\Testing\TestCase
 {
     use RefreshDatabase;
     use DatabaseMigrations;
 
-    /**
-     * @throws BindException
-     */
+    public function createApplication()
+    {
+        $app = new Application();
+
+        $app->singleton(
+            \Illuminate\Contracts\Http\Kernel::class,
+            \Illuminate\Foundation\Http\Kernel::class
+        );
+
+        $app->singleton(
+            \Illuminate\Contracts\Console\Kernel::class,
+            \Illuminate\Foundation\Console\Kernel::class
+        );
+
+        $app->singleton(
+            \Illuminate\Contracts\Debug\ExceptionHandler::class,
+            \Illuminate\Foundation\Exceptions\Handler::class
+        );
+
+
+        $app->register(LaravelGGServiceProvider::class);
+
+        \Illuminate\Support\Facades\Facade::setFacadeApplication($app);
+
+        $app->singleton('config', function ($app) {
+            return new \Illuminate\Config\Repository;
+        });
+
+        return $app;
+    }
+
     protected function setUp(): void
     {
         parent::setUp();
 
-        $this->app = new \Illuminate\Foundation\Application();
-        $this->provider = new \Beaverlabs\LaravelGG\LaravelGGServiceProvider($this->app);
-        $this->provider->register();
+        $config = $this->app->make(Repository::class);
+
+        $config->set('database.default', 'sqlite');
+        $config->set('database.connections.sqlite.database', ':memory:');
+        $config->set('database.connections.sqlite.prefix', '_test');
     }
 }
